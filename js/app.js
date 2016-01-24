a7364814c461681a3afaf81733536874eef93c61
Kissa1001 <kissa101295@gmail.com>
2016-01-23 23:12:54 -0500

    characters created

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+//Animations
+//intro0-text
+$(".intro0-text").typed({
+    strings: [" ", "With more than 1 billion members and counting, Facebook is one of the fastest-growing phenomena to come out of a college dorm room. From Web-based applications to pokes, from photo uploads to fan pages, there's something about this social networking service that sucks people in and won't let them go. What do you know about Facebook?"],
+    typeSpeed: 10
+});
+
+
+
+
 var Quizz = function(questions){
 	this.questions = questions;
 	this.currentQuestionIndex = 0;
@@ -8,18 +18,12 @@ var Quizz = function(questions){
 	});
 	this.score = 0;
 };
+
 Quizz.prototype.currentQuestion = function() {
 	return this.questions[this.currentQuestionIndex];
 };
 
-Quizz.prototype.start = function (){
-	//show the game
-	this.show();
-	//show the 1st question
-	this.currentQuestion().show();
-};
-
-Quizz.prototype.show = function (){
+Quizz.prototype.show = function(){
 	$('body').append('<div id="quizz"></div>');
 };
 
@@ -33,6 +37,7 @@ Quizz.prototype.next = function(){
 		this.end();
 	}
 };
+
 Quizz.prototype.end = function() {
 	console.log('Game Over ' + 'Score is ' + this.score );
 	$('#quizz').append('<h1>Game Over</h1> ' +'Your score is ' + this.score + ' of '+ this.questions.length);
@@ -60,25 +65,91 @@ Question.prototype.show = function(){
 	});
 };
 	var question1 = new Question ({
-		question: 'What is your name?',
-		choices: ['A','B','C'],
-		answer: 'Lina'
+		question: 'Who founded Facebook?',
+		choices: ['Steve Jobs','Mortimer Zuckerman','Barry Zuckercorn'],
+		answer: 'Mark Zuckerberg'
 	});
 	var question2 = new Question ({
-		question: 'What is your age?',
-		choices: ['A','B','C'],
-		answer: '21'
+		question: 'How much did Facebook pay to get its current domain name?',
+		choices: ['$2000','$20,000','$2,000,000'],
+		answer: '$200,000'
 	});
 	var question3 = new Question ({
-		question: 'What are you from?',
-		choices: ['A','B','C'],
-		answer: 'Ukraine'
+		question: 'Which major software company bought a stake in Facebook in 2007?',
+		choices: ['Google','Yahoo','Apple'],
+		answer: 'Microsoft'
+	});
+	var question4 = new Question ({
+		question: 'Where are Facebooks main offices located?',
+		choices: ['Harvard','San Francisco','Silicon Valley'],
+		answer: 'Palo Alto'
+	});
+	var question5 = new Question ({
+		question: 'About 30 million people access their Facebook profile using what?',
+		choices: ['laptops','computers','minds'],
+		answer: 'mobile phones'
 	});
 	var quizz = new Quizz ([
 		question1,
 		question2,
-		question3
+		question3,
+		question4,
+		question5
 	]);
+
+
+var Character =  function (character) {
+	this.intro = character.intro;
+	this.stand = character.stand;
+	this.correct = character.correct;
+	this.wrong = character.wrong;
+	this.fail = character.fail;
+	this.win = character.win;
+};
+var Mugsy = new Character ({
+	intro: $('#mugsy.intro'),
+	stand: $('#mugsy.stand'),
+	correct: $('#mugsy.correct'),
+	wrong: $('#mugsy.wrong'),
+	fail: $('#mugsy.fail'),
+	win: $('#mugsy.win')
+});
+var Mango = new Character ({
+	intro: $('#mango.intro'),
+	stand: $('#mango.stand'),
+	correct: $('#mango.correct'),
+	wrong: $('#mango.wrong'),
+	fail: $('#mango.fail'),
+	win: $('#mango.win')
+});
+
+Quizz.prototype.start = function(){
+ 	//intro0 section appear
+	$('#intro0').fadeIn(500);
+//intro section appear
+	$('.btn-danger').on('click',function(){
+	$('#intro0').hide();
+	$('#intro').fadeIn(1000);
+	var template = Handlebars.compile($('#characters-template').html());
+		$('#intro.container').append(template);
+	});
+	//show the game
+	$('.intro').on('click',function(){
+		switch(characters) {
+    	case Mugsy:
+        	$('#mugsy').show();
+        	$('#mango').hide();
+       		break;
+    	case Mango:
+        	$('#mango').show();
+        	$('#mugsy').hide();
+        	break;
+		}
+	this.show();
+	//show the 1st question
+	this.currentQuestion().show();
+	});
+}; 
 
 	quizz.start();
 });
